b1655bb407165548dee4a12eb9ae508f92cc8e44
zoe-xin <70681059+zoe-xin@users.noreply.github.com>
2021-01-08 15:16:13 +0800

    Update check18s.php

--- check18s.php
+++ check18s.php
@@ -1,7 +1,7 @@
 <?php
         $t = null;
         $num_pass = 18;
-        $t = system('cat /var/log/apache2/other_vhosts_access.log | grep "/upload/pass19check" |grep "php"');
+        $t = system('cat /var/log/apache2/other_vhosts_access.log | grep "/upload/pass18check" |grep "php"');
         if (!$t == null){
                 echo "恭喜你成功利用漏洞，获得本题分数！";
                 include 'send_score.php';
